bac5e9908fa37f959a1d4cb0b104b63e74d8e6b8
devgustavor - Trampo <devgustavor@gmail.com - Trampo>
2023-10-24 18:24:26 -0300

    error: folder web

---
+++ web/src/app/components/Login.tsx
@@ -0,0 +1,16 @@
+export default function Login(){
+  return(
+    <>
+    <section>
+      <div>
+        <h1>Hello world</h1>
+        </div>{/* Icon do topo*/}
+      <div>
+      <div></div>{/*Input de usuario */}
+      <div></div>{/*Input de senhha */}
+      </div>
+    </section>
+  </>  
+  )
+  
+}
